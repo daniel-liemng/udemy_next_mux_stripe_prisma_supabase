d545dcea9101b062c203fc436407ce3fbff5bc5c
Daniel <daniel.liemng@gmail.com>
2023-10-08 15:54:32 -0400

    navbar

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -3,14 +3,17 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <Image
-      src='/logo.svg'
-      alt='logo'
-      width={130}
-      height={130}
-      className='w-16 h-8 object-contain'
-      priority
-    />
+    <div className='flex gap-2 items-center'>
+      <Image
+        src='/logo.svg'
+        alt='logo'
+        width={130}
+        height={130}
+        className='w-16 h-8 object-contain'
+        priority
+      />
+      <h2 className='text-xl text-sky-600 font-semibold'>Udemy</h2>
+    </div>
   );
 };
 
