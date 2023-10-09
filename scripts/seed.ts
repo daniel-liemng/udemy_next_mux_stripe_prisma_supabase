2fb7c48c2cc3f651aa717533878005e057938802
Daniel <daniel.liemng@gmail.com>
2023-10-09 11:07:04 -0400

    category seeding

---
+++ scripts/seed.ts
@@ -0,0 +1,26 @@
+const { PrismaClient } = require('@prisma/client');
+
+const database = new PrismaClient();
+
+const main = async () => {
+  try {
+    await database.category.createMany({
+      data: [
+        { name: 'Computer Science' },
+        { name: 'Music' },
+        { name: 'Fitness' },
+        { name: 'Photography' },
+        { name: 'Accounting' },
+        { name: 'Engineering' },
+        { name: 'Filming' },
+      ],
+    });
+    console.log('Seeding Category Success');
+  } catch (err) {
+    console.log('Error seeding the database categories', err);
+  } finally {
+    await database.$disconnect;
+  }
+};
+
+main();
